4faf9e2f194175d6b2816f11312d2c13531a05f1
longjiang.he <longjiang.he@NCQLONGJIANG-H>
2020-06-02 18:14:30 +0800

    helloworld!.....

---
+++ winfromtest/winfromtest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winfromtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Do you love me?");
+        }
+    }
+}
